791e505c90376b19c61019ff4324b20d35e1aacc
Stephen Wiggins <steve.wiggins7@gmail.com>
2019-03-30 21:32:52 -0400

    links and stuff

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ new Vue({
     },
     mounted () {
       axios
-        .get('./settings.json')
+        .get('https://gist.githubusercontent.com/Avendor7/c2ee64a8fdab047766f1a54b9b0ba592/raw/aa43e11241ab8579c8f0ff8bc07c9d2646b68e83/settings.json')
         .then(response => (this.info = response))
     }
   })
