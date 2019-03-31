41cdd26d61cd68e654ce4aa224f7ea7abd82914b
Stephen <steve.wiggins7@gmail.com>
2019-03-31 19:24:47 -0400

    update settings.json

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ new Vue({
     },
     mounted () {
       axios
-        .get('https://gist.githubusercontent.com/Avendor7/c2ee64a8fdab047766f1a54b9b0ba592/raw/aa43e11241ab8579c8f0ff8bc07c9d2646b68e83/settings.json')
+        .get('https://gist.githubusercontent.com/Avendor7/c2ee64a8fdab047766f1a54b9b0ba592/raw/7972b189af631e9893029937cfc59b3798231e60/settings.json')
         .then(response => (this.results = response.data))
     }
   })
