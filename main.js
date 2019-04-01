604f80f75b20a5148fa4ea9c806372b7c87aa833
Stephen <steve.wiggins7@gmail.com>
2019-03-31 20:01:54 -0400

    links are displaying, needs css

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 new Vue({
-    el: '#example-1',
+    el: '#addresses',
     data () {
       return {
-        results: null
+        addresses: []
       }
     },
     mounted () {
       axios
-        .get('https://gist.githubusercontent.com/Avendor7/c2ee64a8fdab047766f1a54b9b0ba592/raw/7972b189af631e9893029937cfc59b3798231e60/settings.json')
-        .then(response => (this.results = response.data))
+        .get('https://gist.githubusercontent.com/Avendor7/c2ee64a8fdab047766f1a54b9b0ba592/raw/d95e708e784f3f0495682c4fdd7f9ea28d83a60d/settings.json')
+        .then(response => (this.addresses = response.data.addresses))
     }
   })
 
