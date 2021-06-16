bdef34835cb180ccfbd1fe1f4c1deba3f0772406
Stephen <steve.wiggins7@gmail.com>
2021-06-15 23:44:12 -0400

    Always grab the settings from the master branch

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 new Vue({
     el: '#addresses',
-    data () {
-      return {
-        addresses: []
-      }
+    data() {
+        return {
+            addresses: []
+        }
     },
-    mounted () {
-      axios
-        .get('https://gist.githubusercontent.com/Avendor7/c2ee64a8fdab047766f1a54b9b0ba592/raw/d95e708e784f3f0495682c4fdd7f9ea28d83a60d/settings.json')
-        .then(response => (this.addresses = response.data.addresses))
+    mounted() {
+        axios
+            .get('https://raw.githubusercontent.com/Avendor7/Avendor7.github.io/master/settings.jsons')
+            .then(response => (this.addresses = response.data.addresses))
     }
-  })
-
-  
+})
