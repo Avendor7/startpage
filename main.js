52a76cf228d07617c22621bb2b61c14dfeda3e83
Stephen <steve.wiggins7@gmail.com>
2021-06-15 23:48:28 -0400

    can't always grab newest? weird

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ new Vue({
     },
     mounted() {
         axios
-            .get('https://raw.githubusercontent.com/Avendor7/Avendor7.github.io/master/settings.jsons')
+            .get('https://raw.githubusercontent.com/Avendor7/Avendor7.github.io/5044ab01e55bb141a99a78c4403195d9b4cac623/settings.json')
             .then(response => (this.addresses = response.data.addresses))
     }
 })
